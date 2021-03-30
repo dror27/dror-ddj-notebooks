dd42facaee4ea2c083e4e9fb031711be8ba64247
Dror Kessler <dror27.kessler@gmail.com>
2021-03-30 19:38:03 +0100

    consolidated compare file into a single file with an extra column name cmp

--- scraping/completelyretail.co.uk/cmp_props.sh
+++ scraping/completelyretail.co.uk/cmp_props.sh
@@ -4,17 +4,13 @@ F1=$1
 F2=$2
 FO=$3
 
-# lines only in F1
-head -1 $F1 >"${FO}_f1.csv"
-comm -23 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F1) -  >>"${FO}_f1.csv"
-
-# lines only in F2
-head -1 $F2 >"${FO}_f2.csv"
-comm -13 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F2) -  >>"${FO}_f2.csv"	
-
-# lines in both in F1 and F2
-head -1 $F2 >"${FO}_f3.csv"
-comm -12 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F2) -  >>"${FO}_f3.csv"		
+# generate a combined file with an extra column
+# column 'reduced' no longer populated on F2 so we are adding an empty value
+head -1 $F2 | awk 'BEGIN {OFS=","} {print $0,"cmp"}' >$FO.csv
+cat <(comm -23 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
+	| join -t ',' -1 1 -2 1 <(sort $F1) - | awk 'BEGIN {OFS=","} {print $0,1}') \
+    <(comm -13 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
+	| join -t ',' -1 1 -2 1 <(sort $F2) - | awk 'BEGIN {OFS=","} {print $0,"",2}') \
+    <(comm -12 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
+	| join -t ',' -1 1 -2 1 <(sort $F2) - | awk 'BEGIN {OFS=","} {print $0,"",3}') \
+	| grep -v "latitude,longitude" | sort -n -t ',' -k 1 >>$FO.csv
