a46b54e7e7efc950291d3144c5d61369a97ef7e7
Dror Kessler <dror27.kessler@gmail.com>
2021-03-30 19:16:20 +0100

    header line added to compare results

--- scraping/completelyretail.co.uk/cmp_props.sh
+++ scraping/completelyretail.co.uk/cmp_props.sh
@@ -5,13 +5,16 @@ F2=$2
 FO=$3
 
 # lines only in F1
+head -1 $F1 >"${FO}_f1.csv"
 comm -23 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F1) -  >"${FO}_f1.csv"
+	| join -t ',' -1 1 -2 1 <(sort $F1) -  >>"${FO}_f1.csv"
 
 # lines only in F2
+head -1 $F2 >"${FO}_f2.csv"
 comm -13 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F2) -  >"${FO}_f2.csv"	
+	| join -t ',' -1 1 -2 1 <(sort $F2) -  >>"${FO}_f2.csv"	
 
 # lines in both in F1 and F2
+head -1 $F2 >"${FO}_f3.csv"
 comm -12 <(cut -d ',' -f 1 <$F1 | sort) <(cut -d ',' -f 1 <$F2 | sort) \
-	| join -t ',' -1 1 -2 1 <(sort $F2) -  >"${FO}_f3.csv"		
+	| join -t ',' -1 1 -2 1 <(sort $F2) -  >>"${FO}_f3.csv"		
